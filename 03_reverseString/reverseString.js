b9e05f088c34b7ce73eb429afd8921350833dcb5
Joel D'Autilio <joeldautilio@gmail.com>
2022-12-08 16:57:26 -0300

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let result = "";
+  let strArray = str.split(""); // Turn the string into an array
 
+  for(let i = str.length-1; i >= 0; i--)
+    result += strArray[i];
+
+  return result;
 };
 
 // Do not edit below this line
