89511f427bf674f18ce299e527e5eccb13ec0e22
Joel D'Autilio <joeldautilio@gmail.com>
2022-12-08 17:36:53 -0300

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+  if(typeof n1 !== "number" || typeof n2 !== "number") return "ERROR";
 
+  if(n1 < 0 || n2 < 0) return "ERROR";
+
+  let init = (n1 <= n2) ? n1 : n2;
+  let end = (n1 > n2) ? n1 : n2;
+  let result = 0;
+
+  for(let i = init; i <= end; i++)
+    result += i;
+
+  return result;
 };
 
 // Do not edit below this line
