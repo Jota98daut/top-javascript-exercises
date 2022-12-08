164a3a70f5b3be8c54a9e31a1253eda4ada69013
Joel D'Autilio <joeldautilio@gmail.com>
2022-12-08 17:29:14 -0300

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  let newArray = [...array];
+  args.forEach(e => {
+    for(let i = newArray.length-1; i >= 0; i--) {
+      if(newArray[i] === e)
+        newArray.splice(i,1);
+    }
+  });
 
+  return newArray;
 };
 
 // Do not edit below this line
