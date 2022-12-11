461b0f1ead541b824c88bd5693a4fa3a0ec29591
Joel D'Autilio <joeldautilio@gmail.com>
2022-12-11 20:34:37 -0300

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const date = new Date();
+  const sorted = people.sort(
+    (personA, personB) => {
+      personALastYear = personA.yearOfDeath || date.getFullYear();
+      personBLastYear = personB.yearOfDeath || date.getFullYear();
+      const personAAge = personALastYear - personA.yearOfBirth;
+      const personBAge = personBLastYear - personB.yearOfBirth;
+      return (personAAge < personBAge) ? 1 : -1;
+    }
+  )
+  return sorted[0];
 };
 
 // Do not edit below this line
