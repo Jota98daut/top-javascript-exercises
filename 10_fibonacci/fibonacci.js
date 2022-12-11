765e0c3b7aea50dceb9f4e1870ae621831efa700
Joel D'Autilio <joeldautilio@gmail.com>
2022-12-11 19:30:38 -0300

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if(n < 0) return "OOPS";
 
+  function fibAux(n, m, l) {
+    if(l == 0) return m;
+    else
+      return fibAux(m, m+n, l-1);
+  }
+
+  return fibAux(1, 0, n);
 };
 
 // Do not edit below this line
