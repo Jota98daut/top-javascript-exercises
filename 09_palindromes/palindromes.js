d307800a3be15e23fa655a6ae9ff3c82e0c5cfb1
Joel D'Autilio <joeldautilio@gmail.com>
2022-12-11 19:14:55 -0300

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (s) {
+  s = prepare(s);
 
+  for(let i = 0; i < s.length / 2; i++)
+    if(s[i] !== s[s.length-1-i]) return false;
+
+  return true;
 };
+
+function prepare(s) {
+  for(c of s)
+    if(!(/\w/.test(c))) s = s.replace(c,"");
+  return s.toLowerCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
